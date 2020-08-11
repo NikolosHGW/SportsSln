afab5d0cb03b464cdd68e6ede1c0e59e8b4cba7f
Ник Николос <nic23172@gmail.com>
2020-08-11 13:13:41 +0300

    Before Building a Category Navigation Menu

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -14,15 +14,27 @@ namespace SportsStore.Controllers
             repository = repo;
         }
         //public IActionResult Index() => View(repository.Products);
-        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
+        //public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
+        //{
+        //    Products = repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
+        //    PagingInfo = new PagingInfo
+        //    {
+        //        CurrentPage = productPage,
+        //        ItemsPerPage = PageSize,
+        //        TotalItems = repository.Products.Count()
+        //    }
+        //});
+        public ViewResult Index(string category, int productPage = 1) => View(new ProductsListViewModel
         {
-            Products = repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
+            Products = repository.Products.Where(p => category == null || p.Category == category)
+            .OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
             PagingInfo = new PagingInfo
             {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
                 TotalItems = repository.Products.Count()
-            }
+            },
+            CurrentCategory = category
         });
     }
 }
