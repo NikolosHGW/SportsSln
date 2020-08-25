c2a48a038af3bfcd5405e836a2b97a965da839d3
Ник Николос <nic23172@gmail.com>
2020-08-25 10:45:13 +0300

    Before CHAPTER 9

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -22,8 +22,7 @@ namespace SportsStore.Pages
         }
         public IActionResult OnPost(long productId, string returnUrl)
         {
-            Product product = repository.Products
-            .FirstOrDefault(p => p.ProductID == productId);
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
             HttpContext.Session.SetJson("cart", Cart);
